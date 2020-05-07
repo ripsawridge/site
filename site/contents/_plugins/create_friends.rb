4723d46721d3f5d3cc4b5acee1a345a4c77e43a1
Mike Stanton <mvstanton@chromium.org>
2020-05-07 13:37:36 +0200

    friends now works

--- site/contents/_plugins/create_friends.rb
+++ site/contents/_plugins/create_friends.rb
@@ -5,14 +5,37 @@ require 'json'
 module Jekyll
   class CreateFriends < Jekyll::Generator
 
+    def addGuest(guests, guest)
+      if guests[guest]
+        return
+      end
+      guests[guest] = {}
+    end
+
     def generate(site)
       # Create one simple friend page for now.
       @site = site
       dir = "friends"
-      friend = "georg"
-      site.pages << FriendPage.new(site, site.source, \
-                                   File.join(dir, friend), friend)
-      Jekyll.logger.warn "CreateFriends", "I've been called"
+
+      guests = {}
+      trips = @site.collections["cma"].docs
+      trips.each do |trip|
+        trip_guests = trip.data["guests"]
+        if trip_guests.is_a?(Array)
+          trip_guests.each do |trip_guest|
+            addGuest(guests, trip_guest)
+          end
+        elsif trip_guests
+          addGuest(guests, trip_guests)
+        end
+      end
+
+      # Create a page for each unique guest.
+      guests.keys.each  do |guest|
+        site.pages << FriendPage.new(site, site.source, \
+                                     File.join(dir, guest), guest)
+        Jekyll.logger.warn "CreateFriends", guest
+      end
     end
   end
 
@@ -27,6 +50,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, "_layouts"), 'friend.html')
       self.data['friend'] = friend
+      self.data["friend_description"] = "Here are my reports with " + friend + "."
       self.data["title"] = "My friend " + friend
     end
   end
